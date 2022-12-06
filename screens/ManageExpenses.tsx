e969f7745297ac05ab6c4ca7972c2daeb4b849b0
이다원 <2da1@idawon-ui-MacBookPro.local>
2022-12-06 09:23:21 +0900

    [Feat] 네비게이션 구성하기

---
+++ screens/ManageExpenses.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+const ManageExpenses = () => {
+  return (
+    <>
+      <Text>Manage</Text>
+    </>
+  );
+};
+
+export default ManageExpenses;
---
+++ screens/RecentExpenses.tsx
@@ -0,0 +1,11 @@
+import { Text } from "react-native";
+
+const RecentExpenses = () => {
+  return (
+    <>
+      <Text>Recent</Text>
+    </>
+  );
+};
+
+export default RecentExpenses;
